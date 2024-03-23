2db3fcfd13d7ac0e0dbf0a3147458d0ec56963e0
Alejandro Rosa <pedroalejandrorosaperez@gmail.com>
2024-03-23 11:31:15 -0400

    Actualizar network.js

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const response = require('../../../network/response');
 router.get('/', function(req, res){
     res.send('Todo funciona');
+    response.success.(req,res,'todo va bien', 200)
 });
 
 module.exports = router;
